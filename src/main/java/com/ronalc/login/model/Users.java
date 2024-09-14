b77c8626eb8b15e2d4b6723bfacd22d23595fb8c
Ronaldo de Alcantara <ronaldothealcantara@gmail.com>
2024-09-14 11:34:11 -0300

    Refactoring Users and Include Prometheus

--- src/main/java/com/ronalc/login/model/Users.java
+++ src/main/java/com/ronalc/login/model/Users.java
@@ -1,13 +1,25 @@
 package com.ronalc.login.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "usuarios")
 public class Users {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(name = "usuario")
     private String username;
+    @Column(name = "senha")
     private String password;
 }
